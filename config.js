8a52d3e9b395dcfb5b08679bfa3618d892cf2faa
hebotkr <heprabathmdacc@gmail.com>
2024-11-10 18:53:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ SESSION_ID: process.env.SESSION_ID || "TRgQRT4K#0C--uKcQDhSZsjn_Kke0V_pB_cape8_R
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
-MODE: process.env.MODE || "private", //private or public
+MODE: process.env.MODE || "public", //private or public
 AUTO_VOICE:"true" //true or false
 };
