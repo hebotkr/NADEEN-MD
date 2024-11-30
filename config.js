b517bec171d2cbfc93ee6b7cff5b76e35c057e63
hebotkr <heprabathmdacc@gmail.com>
2024-11-30 14:11:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "WEx1nApb#Zv5n2pMUlYKYqkCB0F7aKoVCoC8njzhdjF10hvuRyso ", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*🌝 Hello I am nadeen KRP*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
-MODE: process.env.MODE || "public", //private or public
+MODE: process.env.MODE || "private", //private or public
 AUTO_VOICE:"true" //true or false
 };
