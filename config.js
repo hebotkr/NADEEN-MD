c472afae685d1684d854fdf00ea5a6764edf61fa
hebotkr <heprabathmdacc@gmail.com>
2024-12-17 20:24:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "WEx1nApb#Zv5n2pMUlYKYqkCB0F7aKoVCoC8njzhdjF10hvuRyso ", //put your session id
+SESSION_ID: process.env.SESSION_ID || "6BdEyIrI#gkn6rr2dAb9WnhK3c2DEoZGfx2tRgcICtZJKmUO2mvM", //put you session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*🌝 Hello I am kavi*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
